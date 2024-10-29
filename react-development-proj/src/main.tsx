18e67b76dc215bcac1c603034e63fff250b29923
Millie <57187866+unamimo@users.noreply.github.com>
2024-10-29 15:46:12 +0000

    first commit

---
+++ react-development-proj/src/main.tsx
@@ -0,0 +1,21 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App';
+
+const router = createBrowserRouter([
+  {
+    path: "/",          // route path
+    element: <App />,   // tells the router where to start
+    children: [
+        {path: "", element: <App />, }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
