9445b38d6d4941251b928f094dcbcd9b13ff58c8
Millie <57187866+unamimo@users.noreply.github.com>
2024-10-30 18:36:09 +0000

    chore: comment out existing code for clarification

--- react-development-proj/src/App.tsx
+++ react-development-proj/src/App.tsx
@@ -4,18 +4,20 @@ function App() {
 
   return (
     <>
-      <h1 style={{border: "2px solid blue"}}>Title</h1>
+    <div style={{ maxWidth: "1280px", maxHeight: "100vh", overflow: "auto", border: "2px solid green" }}>
+      <h1 style={{border: "2px solid blue", fontSize: "6.5em"}}>Title</h1>
       <div className="card" style={{border: "2px solid red" }}>
-        <button style={{ margin: "9px" }}>
+        <button className='TitleButtons'>
           Button 1
         </button>
-        <button style={{ margin: "9px" }}>
+        <button className='TitleButtons'>
           Button 2
         </button>
-        <button style={{ margin: "9px" }}>
+        <button className='TitleButtons'>
           Button 3
         </button>
       </div>
+    </div>
     </>
   )
 }
