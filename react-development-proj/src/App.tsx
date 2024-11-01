25d8798ab74a77c7948756b287000f59ffb4bf69
Millie <57187866+unamimo@users.noreply.github.com>
2024-11-01 15:59:00 +0000

    DP 54: Align Title Screen in centre (#1)
    * style: setup base title screen
    
    Delete base vite logos and text from App.tsx
    
    * chore: change project title
    
    * chore: remove unnecessary CSS
    
    * chore: add borders to objects
    
    Add borders for clarification on div sizing
    
    * chore: comment out existing code for clarification
    
    * style: centre content vertically
    
    * style: centre content vertically and horizontally
    
    after creating a 'layout', centre the content within in
    
    * chore: remove borders used for testing
    
    * chore: Clean up code

--- react-development-proj/src/App.tsx
+++ react-development-proj/src/App.tsx
@@ -1,33 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+// currently includes the title screen code
+// this will need to be seperated out into a layout component for reuse, the actual title screen content will be a child
+// all of this code may be moved into different pages, when a pages folder structure is introduced
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {/*container for everything*/}
+      {/*may include header and footer in the future */}
+      <div style={{
+        display: "flex",
+        height: "100%",
+        width: "100%",
+        flexDirection: "column",
+        minHeight: "100vh",
+      }}>
+        {/* Container for centring content in the middle of the screen
+          - flex: '1 0 auto' - child element grows to fill space and pushes the footer to the bottom
+          - flexGrow makes the content grow to the window size
+          - overflow: auto, means that content does not go beyond its container
+          - minHeight must be 100vh for the footer to be at the bottom of the screen
+          - maxWidth - maxiumum width of our centred content
+          - centred horizontally and vertically using alignItems and justifyContent
+        */}
+        <div style={{
+          flexGrow: 3,
+          overflow: "auto",
+          display: "flex",
+          flexDirection: "column",
+          maxWidth: "1280px",
+          height: "100%",
+          width: "100%",
+          alignSelf: "center",
+          flex: "1 0 auto",
+          alignItems: "center",       // centres child divs as well as content, remove for divs to span
+          justifyContent: "center"    // centres child content within content divs
+        }}>
+            {/* Actual page content */}
+            {/* Move these in-line styles to a CSS file */}
+            <h1 style={{ fontSize: "6.5em", textAlign: "center" }}>Title</h1>
+            <div style={{ textAlign: "center"}}>
+              <button className='TitleButtons'>
+                Button 1
+              </button>
+              <button className='TitleButtons'>
+                Button 2
+              </button>
+              <button className='TitleButtons'>
+                Button 3
+              </button>
+            </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
