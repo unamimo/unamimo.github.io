24be4cd95abca4f694aabb96c8e8e5bc40be66ca
Millie <57187866+unamimo@users.noreply.github.com>
2024-11-01 11:09:23 +0000

    style: centre content vertically

--- react-development-proj/src/App.tsx
+++ react-development-proj/src/App.tsx
@@ -4,20 +4,49 @@ function App() {
 
   return (
     <>
-    <div style={{ maxWidth: "1280px", maxHeight: "100vh", overflow: "auto", border: "2px solid green" }}>
-      <h1 style={{border: "2px solid blue", fontSize: "6.5em"}}>Title</h1>
-      <div className="card" style={{border: "2px solid red" }}>
-        <button className='TitleButtons'>
-          Button 1
-        </button>
-        <button className='TitleButtons'>
-          Button 2
-        </button>
-        <button className='TitleButtons'>
-          Button 3
-        </button>
+      {/*container for everything*/}
+      <div style={{
+        display: "flex",
+        height: "100%",
+        width: "100%",
+        flexDirection: "column",
+        minHeight: "100vh",
+      }}>
+        {/* Container for centring content in the middle of the screen
+          - flex: '1 0 auto' - child element grows to fill space and pushes the footer to the bottom
+          - flexGrow makes the content grow to the window size
+          - overflow: auto, means that content does not go beyond its container
+          - minHeight must be 100vh for the footer to be at the bottom of the screen
+          - maxWidth - maxiumum width of our centred content
+        */}
+        <div style={{
+          border: "2px solid green",
+          flexGrow: 3,
+          overflow: "auto",
+          display: "flex",
+          flexDirection: "column",
+          maxWidth: "1280px",
+          height: "100%",
+          width: "100%",
+          alignSelf: "center",
+          flex: "1 0 auto",
+        }}>
+          <div style={{ flex: "1 0 auto"}}>
+            <h1 style={{border: "2px solid blue", fontSize: "6.5em", textAlign: "center"}}>Title</h1>
+            <div className="card" style={{border: "2px solid red", textAlign: "center"}}>
+              <button className='TitleButtons'>
+                Button 1
+              </button>
+              <button className='TitleButtons'>
+                Button 2
+              </button>
+              <button className='TitleButtons'>
+                Button 3
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
     </>
   )
 }
