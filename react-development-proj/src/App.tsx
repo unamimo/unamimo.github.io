b8a3af15781668ad10a04f980fa0702d80c99233
Millie <57187866+unamimo@users.noreply.github.com>
2024-11-04 10:17:33 +0000

    chore: create layout
    move layout related styling to its own folder
    create pages, layout, utils folder

--- react-development-proj/src/App.tsx
+++ react-development-proj/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import TitleLayout from './layout/TitleLayout'
 
 // currently includes the title screen code
 // this will need to be seperated out into a layout component for reuse, the actual title screen content will be a child
@@ -6,52 +7,22 @@ import './App.css'
 function App() {
   return (
     <>
-      {/*container for everything*/}
-      {/*may include header and footer in the future */}
-      <div style={{
-        display: "flex",
-        height: "100%",
-        width: "100%",
-        flexDirection: "column",
-        minHeight: "100vh",
-      }}>
-        {/* Container for centring content in the middle of the screen
-          - flex: '1 0 auto' - child element grows to fill space and pushes the footer to the bottom
-          - flexGrow makes the content grow to the window size
-          - overflow: auto, means that content does not go beyond its container
-          - minHeight must be 100vh for the footer to be at the bottom of the screen
-          - maxWidth - maxiumum width of our centred content
-          - centred horizontally and vertically using alignItems and justifyContent
-        */}
-        <div style={{
-          flexGrow: 3,
-          overflow: "auto",
-          display: "flex",
-          flexDirection: "column",
-          maxWidth: "1280px",
-          height: "100%",
-          width: "100%",
-          alignSelf: "center",
-          flex: "1 0 auto",
-          alignItems: "center",       // centres child divs as well as content, remove for divs to span
-          justifyContent: "center"    // centres child content within content divs
-        }}>
-            {/* Actual page content */}
-            {/* Move these in-line styles to a CSS file */}
-            <h1 style={{ fontSize: "6.5em", textAlign: "center" }}>Title</h1>
-            <div style={{ textAlign: "center"}}>
-              <button className='TitleButtons'>
-                Button 1
-              </button>
-              <button className='TitleButtons'>
-                Button 2
-              </button>
-              <button className='TitleButtons'>
-                Button 3
-              </button>
-            </div>
+      <TitleLayout>
+        {/* Actual page content */}
+        {/* Move these in-line styles to a CSS file */}
+        <h1 style={{ fontSize: "6.5em", textAlign: "center" }}>Title</h1>
+        <div style={{ textAlign: "center"}}>
+          <button className='TitleButtons'>
+            Button 1
+          </button>
+          <button className='TitleButtons'>
+            Button 2
+          </button>
+          <button className='TitleButtons'>
+            Button 3
+          </button>
         </div>
-      </div>
+      </TitleLayout>
     </>
   )
 }
---
+++ react-development-proj/src/layout/TitleLayout.tsx
@@ -0,0 +1,52 @@
+import { PropsWithChildren, ReactNode } from "react";
+
+// currently includes the title screen code
+// this will need to be seperated out into a layout component for reuse, the actual title screen content will be a child
+
+// all of this code may be moved into different pages, when a pages folder structure is introduced
+
+interface TitleLayoutProps {
+  children?: ReactNode;
+}
+
+export default function TitleLayout({
+  children
+}: PropsWithChildren<TitleLayoutProps>) {
+    return (
+      <>
+        {/*container for everything*/}
+        {/*may include header and footer in the future */}
+        <div style={{
+          display: "flex",
+          height: "100%",
+          width: "100%",
+          flexDirection: "column",
+          minHeight: "100vh",
+        }}>
+          {/* Container for centring content in the middle of the screen
+            - flex: '1 0 auto' - child element grows to fill space and pushes the footer to the bottom
+            - flexGrow makes the content grow to the window size
+            - overflow: auto, means that content does not go beyond its container
+            - minHeight must be 100vh for the footer to be at the bottom of the screen
+            - maxWidth - maxiumum width of our centred content
+            - centred horizontally and vertically using alignItems and justifyContent
+          */}
+          <div style={{
+            flexGrow: 3,
+            overflow: "auto",
+            display: "flex",
+            flexDirection: "column",
+            maxWidth: "1280px",
+            height: "100%",
+            width: "100%",
+            alignSelf: "center",
+            flex: "1 0 auto",
+            alignItems: "center",       // centres child divs as well as content, remove for divs to span
+            justifyContent: "center"    // centres child content within content divs
+          }}>
+            {children}
+          </div>
+        </div>
+      </>
+    )
+  }
