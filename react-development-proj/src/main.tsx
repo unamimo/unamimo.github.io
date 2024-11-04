9e3df37725578fb351f769d4db99c46126a6a531
Millie <57187866+unamimo@users.noreply.github.com>
2024-11-04 10:33:21 +0000

    chore: move App file to pages folder

--- react-development-proj/src/main.tsx
+++ react-development-proj/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './App';
+import App from './pages/App/App';
 
 const router = createBrowserRouter([
   {
--- react-development-proj/src/pages/App/App.tsx
+++ react-development-proj/src/pages/App/App.tsx
@@ -1,11 +1,11 @@
 import './App.css'
-import TitleLayout from './layout/TitleLayout'
+import TitleLayout from '../../layout/TitleLayout'
 
 function App() {
   return (
     <>
       <TitleLayout>
-        {/* Move these in-line styles to a CSS file */}
+        {/* Move these in-line styles to a CSS file? Could get messy in the future */}
         <h1 style={{ fontSize: "6.5em", textAlign: "center" }}>Title</h1>
         <div style={{ textAlign: "center"}}>
           <button className='TitleButtons'>
