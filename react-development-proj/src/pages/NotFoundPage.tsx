2c81cd136c416340a235153f9fbe05eaa1682cf9
Millie <57187866+unamimo@users.noreply.github.com>
2024-11-04 11:02:06 +0000

    feat: route to new placeholder pages
    additionally, create a 404 page

---
+++ react-development-proj/src/pages/NotFoundPage.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export default function NotFoundPage () {
+    return(
+        <>
+            <div>404 Not Found</div>
+            <div>
+                <Link to="/">Go to the home page</Link>
+            </div>
+        </>
+    )
+}
