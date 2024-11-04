4a8bf218a1738ff6a808a8c48061205942c4fd8a
Millie <57187866+unamimo@users.noreply.github.com>
2024-11-04 11:26:34 +0000

    chore: add comments to link component

--- react-development-proj/src/components/MyLink.tsx
+++ react-development-proj/src/components/MyLink.tsx
@@ -1,6 +1,10 @@
 import { ReactNode } from "react";
 import { Link } from "react-router-dom";
 
+// create custom link component to change the default styling
+// this will most likely be replaced by focus, hover and visited global styling in the future
+// if global styling is applied, this component may become redundant in the future
+
 interface MyLinkProps {
     to: string;
     children: ReactNode
