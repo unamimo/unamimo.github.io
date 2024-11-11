746a5ed1fe56ae54bd139b5fc53c545a1f459d08
Millie <57187866+unamimo@users.noreply.github.com>
2024-11-11 13:00:49 +0000

    chore: update tts toggle with new hook

--- react-development-proj/src/components/TextToSpeechToggle.tsx
+++ react-development-proj/src/components/TextToSpeechToggle.tsx
@@ -1,21 +1,21 @@
-import { voiceText } from "./TextToSpeech";
 import { useTextToSpeech } from "../context/TextToSpeechContext";
-
+import { useVoice } from "./useVoice";
 
 // this component is used for turning text to speech on and off
 export default function TextToSpeechToggle() {
     const { isNarratorEnabled, turnOn, turnOff } = useTextToSpeech();
+    const voiceText = useVoice();
 
     return(
         <>
             {/* Render enable button if the narrator is off, and vise versa */}
             {isNarratorEnabled ?  (
-                <button onClick={() => turnOff()}>Disable narrator</button>
+                <button onClick={() => turnOff()} onFocus={() => voiceText("Disable narrator")}>Disable narrator</button>
             ) : (
                 <button onClick={() => turnOn()}>Enable narrator</button>
             )
             }
-            {voiceText("Narrator enabled")}
+            <button onClick={() => voiceText("I am a test")}>I am a test</button>
         </>
     )
 }
