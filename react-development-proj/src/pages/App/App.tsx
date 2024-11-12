454ccd1d47e1100cd4a0bf7ce5c04d3f44982afc
Millie <57187866+unamimo@users.noreply.github.com>
2024-11-12 10:18:16 +0000

    fix: aria has no main landmark
    - assign roles to divs, to provide landmarks for users using screen
      readers

--- react-development-proj/src/pages/App/App.tsx
+++ react-development-proj/src/pages/App/App.tsx
@@ -5,13 +5,13 @@ import NarratedText from '../../components/NarratedText'
 
 function App() {
   return (
-    <>
+    <div role='main'>
       <TitleLayout>
         {/* Move these in-line styles to a CSS file? Could get messy in the future */}
         <h1 style={{ fontSize: "6.5em", textAlign: "center" }}>
           <NarratedText text="Title"/>
         </h1>
-        <div style={{ textAlign: "center"}}>
+        <div style={{ textAlign: "center"}} role='navigation'>
           <MyLink to="Page1">
             <button className='TitleButtons'>
               Button 1
@@ -29,8 +29,8 @@ function App() {
           </MyLink>
         </div>
       </TitleLayout>
-    </>
-  )
-}
+    </div>
+  );
+};
 
 export default App
