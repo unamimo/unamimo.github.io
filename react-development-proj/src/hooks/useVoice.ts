8ebccf7ad3a4a6133fe5b9725c7c170ba2a608b3
Millie <57187866+unamimo@users.noreply.github.com>
2024-11-18 12:11:23 +0000

    fix: disable narrator button
    - fix the disable narrator button not working

--- react-development-proj/src/hooks/useVoice.ts
+++ react-development-proj/src/hooks/useVoice.ts
@@ -5,17 +5,20 @@ export function useVoice () {
     const { isNarratorEnabled } = useTextToSpeech();
     
     const voiceText = (text?: string) => {
-        isNarratorEnabled ? handlePlay(text) : handleStop;
+        isNarratorEnabled ? handlePlay(text) : handleStop();
+        console.log("is narrator enabled in voiceText function?: ",isNarratorEnabled)
     }
 
     return voiceText;
 };
 
 function handlePlay(text?: string) {
+    console.log("Entered handle play function")
     const utteranceText = new SpeechSynthesisUtterance(text);
     speechSynthesis.speak(utteranceText);
 };
 
 function handleStop() {
-    window.speechSynthesis.cancel();
+    console.log("Entered stop TTS function")
+    speechSynthesis.cancel();
 };
