137f3b2a883dc70bf868ebc71c5d8315f3a9d355
Millie <57187866+unamimo@users.noreply.github.com>
2024-11-18 15:24:29 +0000

    feat: screen reader will interupt previous speech
    - additionally remove screen reading for current page text, this may be
      renabled later in the future

--- react-development-proj/src/hooks/useVoice.ts
+++ react-development-proj/src/hooks/useVoice.ts
@@ -1,3 +1,6 @@
+// this component controls text to speech based on if the narrator is enabled or not 
+// this uses MDN speech synthesis: https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis
+
 import { useTextToSpeech } from "../context/TextToSpeechContext";
 
 
@@ -13,6 +16,12 @@ export function useVoice () {
 };
 
 function handlePlay(text?: string) {
+    // this if statement makes the narrator interupt itself if it needs to say new text
+    // remove to have the narrator wait before voicing new text
+    // this will need to be removed for pages which want to voice all the text 
+    if (speechSynthesis.speaking) {
+        handleStop();
+    }
     console.log("Entered handle play function")
     const utteranceText = new SpeechSynthesisUtterance(text);
     speechSynthesis.speak(utteranceText);
