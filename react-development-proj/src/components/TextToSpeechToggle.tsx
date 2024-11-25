aa9b74c3cb04c85059b6fbefc157d4529e7a807b
Millie <57187866+unamimo@users.noreply.github.com>
2024-11-25 12:41:05 +0000

    chore: add translations to components

--- react-development-proj/src/components/TextToSpeechToggle.tsx
+++ react-development-proj/src/components/TextToSpeechToggle.tsx
@@ -1,8 +1,10 @@
 import { useTextToSpeech } from "../context/TextToSpeechContext";
 import { useVoice } from "../hooks/useVoice";
+import { useTranslation } from 'react-i18next';
 
 // this component is used for turning text to speech on and off
 export default function TextToSpeechToggle() {
+    const { t } = useTranslation();
     const { isNarratorEnabled, turnOn, turnOff } = useTextToSpeech();
     const voiceText = useVoice();
 
@@ -10,9 +12,9 @@ export default function TextToSpeechToggle() {
         <>
             {/* Render enable button if the narrator is off, and vise versa */}
             {isNarratorEnabled ?  (
-                <button onClick={() => turnOff()} onFocus={() => voiceText("Disable narrator")}>Disable narrator</button>
+                <button onClick={() => turnOff()} onFocus={() => voiceText(t("components.TextToSpeechToggle.disable_narrator"))}>{t("components.TextToSpeechToggle.disable_narrator")}</button>
             ) : (
-                <button onClick={() => turnOn()}>Enable narrator</button>
+                <button onClick={() => turnOn()}>{t("components.TextToSpeechToggle.enable_narrator")}</button>
             )
             }
         </>
