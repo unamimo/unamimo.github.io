6e3912225caf166bd1e4201d9e18dc621f2c029b
Millie <57187866+unamimo@users.noreply.github.com>
2024-11-28 11:18:02 +0000

    fix: narrator says language toggle buttons

--- react-development-proj/src/components/ToggleLanguage.tsx
+++ react-development-proj/src/components/ToggleLanguage.tsx
@@ -1,21 +1,24 @@
 import { useTranslation } from "react-i18next";
 import i18n from "../i18n";
+import { useVoice } from "../hooks/useVoice";
 
 export default function ToggleLanguage() {
     const { t } = useTranslation();
+    const voiceText = useVoice();
+
 
     return(
         <div>
             <p aria-label="change-language">
                 {t("components.ToggleLanguage.description")}
             </p>
-            <button onClick={ () => i18n.changeLanguage('en')} aria-labelledby="change-language">
+            <button onClick={ () => i18n.changeLanguage('en')} onFocus={() => voiceText(t("components.ToggleLanguage.english"))} aria-labelledby="change-language">
                 {t("components.ToggleLanguage.english")}
             </button>
-            <button onClick={ () => i18n.changeLanguage('es')} aria-labelledby="change-language">
+            <button onClick={ () => i18n.changeLanguage('es')} onFocus={() => voiceText(t("components.ToggleLanguage.spanish"))} aria-labelledby="change-language">
                 {t("components.ToggleLanguage.spanish")}
             </button>
-            <button onClick={ () => i18n.changeLanguage('jp')} aria-labelledby="change-language">
+            <button onClick={ () => i18n.changeLanguage('jp')} onFocus={() => voiceText(t("components.ToggleLanguage.japanese"))} aria-labelledby="change-language">
                 {t("components.ToggleLanguage.japanese")}
             </button>
         </div>
