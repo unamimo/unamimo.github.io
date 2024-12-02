5e71bf3055d96f4b4d311754644595aae233e5d2
Millie <57187866+unamimo@users.noreply.github.com>
2024-12-02 10:23:08 +0000

    feat: add about info`

--- react-development-proj/src/pages/About/About.tsx
+++ react-development-proj/src/pages/About/About.tsx
@@ -18,8 +18,22 @@ export default function About (){
                 </Region>
                 <Region aria-labelledby={"About"}>
                     <p>
+                        {/* Remove later */}
                         {t("pages.about.description")}
                     </p>
+                    <p>Features</p>
+                    <ul>
+                        <li>Landmark navigation</li>
+                        <li>Custom made screen reader</li>
+                        <li>Language switcher</li>
+                        <li>Dark theme</li>
+                    </ul>
+                    <p>Accessibility</p>
+                    <ul>
+                        <li>0 issues when testing with AXE (accessibility) tools</li>
+                        <li>Compaitable with screen readers (NVDA)</li>
+                        <li>Compliance with WCAG 2</li>
+                    </ul>
                 </Region>
                 <Navigation>
                     <MyLink renderAsButton={true} to="/">{t("general.home")}</MyLink>
