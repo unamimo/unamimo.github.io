b15cc9ee20312571b4e0d61b5271246974474663
Millie <57187866+unamimo@users.noreply.github.com>
2024-12-24 11:24:05 +0000

    fix: change select to button
    select was causing a few different problems, prefer using buttons

--- react-development-proj/src/components/ToggleTheme.tsx
+++ react-development-proj/src/components/ToggleTheme.tsx
@@ -5,44 +5,18 @@
 import { useTheme } from "../context/ThemeContext";
 import { useTranslation } from 'react-i18next';
 import { useVoice } from "../hooks/useVoice";
-import { useState } from "react";
 
 
 export default function ToggleTheme () {
     const { t } = useTranslation();
     const { changeTheme } = useTheme();
     const voiceText = useVoice();
-    const [ selectedTheme, setSelectedTheme ] = useState("light");
-
-    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        // handles value change
-        setSelectedTheme(event.target.value);
-        // changes theme
-        changeTheme(event.target.value);
-        // voice the value of whatever is currently selected
-        voiceText(t("components.ToggleTheme."+event.target.value))
-    }
 
     return(
         <>
-            {/* Narrator does not work for select component, this is not ideal */}
-            <select aria-label="theme-selection" value={selectedTheme} onChange={handleChange} onFocus={() => voiceText(t("pages.config.theme_settings"))}>
-                <option
-                    value={"light"} 
-                >
-                    {t("components.ToggleTheme.light")}
-                </option>
-                <option 
-                    value={"dark"} 
-                >
-                    {t("components.ToggleTheme.dark")}
-                </option>
-                <option 
-                    value={"highContrast"} 
-                >
-                    {t("components.ToggleTheme.highContrast")}
-                </option>
-            </select>
+            <button onClick={() => changeTheme("light")} onFocus={() => voiceText(t("components.ToggleTheme.light"))}>{t("components.ToggleTheme.light")}</button>
+            <button onClick={() => changeTheme("dark")} onFocus={() => voiceText(t("components.ToggleTheme.dark"))}>{t("components.ToggleTheme.dark")}</button>
+            <button onClick={() => changeTheme("highContrast")} onFocus={() => voiceText(t("components.ToggleTheme.highContrast"))}>{t("components.ToggleTheme.highContrast")}</button>
         </>
     )
 }
